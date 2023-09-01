870a060b3a5f5e2bdc90c83f354ed64d6d075290
Robin Long <r.long@lancaster.ac.uk>
2023-09-01 12:10:50 +0100

    tweak conf.py

--- conf.py
+++ conf.py
@@ -60,6 +60,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+#html_css_files = ["_static/style.css"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -194,8 +195,8 @@ pygments_style = 'sphinx'
 #elif theme == 'sphinx_rtd_theme':
 #    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
-html_theme = "sphinx_rtd_theme"
-html_css_files = ["_static/style.css"]
+#html_theme = "sphinx_rtd_theme"
+#html_css_files = ["_static/style.css"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
