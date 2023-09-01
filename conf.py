6b3b7fc88c3f156eb1cf2603564952eb0d257236
Robin Long <r.long@lancaster.ac.uk>
2023-09-01 13:25:02 +0100

    tweak conf.py

--- conf.py
+++ conf.py
@@ -60,21 +60,14 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-#html_css_files = ["_static/style.css"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+
 html_static_path = ["_static"]
 html_logo = 'logo-lancs.png'
 
+master_doc = 'index'
 
-#source_suffix = '.rst'
-
-#master_doc = 'index'
 pygments_style = 'sphinx'
-#htmlhelp_basename = 'SphinxRestMemodoc'
-
-
-# -- Options for LaTeX output ---------------------------------------------
-
